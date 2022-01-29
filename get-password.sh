9c2d7a09f79c3dc734e7e2f560805253f80c452c
Nikola Djukanovic <nikoladjukanovic1989@gmail.com>
2022-01-29 18:54:20 +0100

    feat: created script for retrieving password from keepass

---
+++ get-password.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+file_path=$(echo $MY_PWD_FILE_LOCATION)
+
+selected=$(echo $MY_KEEPASS | keepassxc-cli ls $file_path | fzf)
+
+echo $MY_KEEPASS | keepassxc-cli clip $file_path "$selected"
