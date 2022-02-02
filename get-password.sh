4feed8ed5819db11a47024dff88dba5e2676a764
Nikola Djukanovic <nikoladjukanovic1989@gmail.com>
2022-02-02 21:58:13 +0100

    fix: updated get-password.sh script

--- get-password.sh
+++ get-password.sh
@@ -6,4 +6,6 @@ file_path=$(echo $MY_PWD_FILE_LOCATION)
 
 selected=$(echo $MY_KEEPASS | keepassxc-cli ls $file_path | fzf)
 
-echo $MY_KEEPASS | keepassxc-cli clip $file_path "$selected"
+timeout_in_seconds=5
+
+echo $MY_KEEPASS | keepassxc-cli clip "$file_path" "$selected" $timeout_in_seconds
